a3372cc1b2672415937841f0d58d7dd76002f703
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2023-12-30 16:55:20 +0530

    foldername chng

--- 05paswdgenrator/src/App.jsx
+++ 05paswdgenrator/src/App.jsx
@@ -22,16 +22,34 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-md  mx-auto shadow-md rounded-lg px-4  bg-gray-700 mt-20">
+      <div className="w-full max-w-md  mx-auto shadow-md text-white rounded-lg px-4  bg-gray-700 mt-20">
         <h1 className="text-white text-center my-3  ">PasswordGenrator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
           <input
             type="text"
             value={pasword}
-            className="outerline-none  w-full py-1   rounded-lg "
+            className="outerline-none  w-full py-1    "
             readOnly
           />
-          <button className="pl-10 text-white">copy</button>
+          <button className="outline-none bg-blue-700 px-4 py-0.5  shrink-0 text-white">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="curesor-pointer "
+              onChange={(e) => {
+                setlength(e.target.value);
+              }}
+            />
+            <lebel>length:{length}</lebel>
+          </div>
+          <div></div>
         </div>
       </div>
     </>
