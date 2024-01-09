f48f951029d6d7f34036d2e9df67e66e0b8aae00
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-09 16:35:21 +0530

    login$singhup

--- 07reactrouter/src/components/LoginReg/Registration.jsx
+++ 07reactrouter/src/components/LoginReg/Registration.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Registration() {
     return (
         <div > <center>
-            <div className="w-full max-w-xs bg-red-200 mt-10">
+            <div className="w-full max-w-xs bg-gray-200 mt-10">
                 <form class="gray-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div class="mb-6">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
