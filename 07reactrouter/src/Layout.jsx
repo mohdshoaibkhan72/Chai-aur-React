11c577cdab6bf67abe1c518d9cbaec14773eb380
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-05-14 23:31:07 +0530

    some changes

--- 07reactrouter/src/Layout.jsx
+++ 07reactrouter/src/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header"
 import Footer from "./components/footer/Footer"
 import React from "react";
+
 function Layout() {
     return <>
 
